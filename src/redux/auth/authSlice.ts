1c1525e35917dcd16b4c9fd83527b3cac7873fd2
Anna Zubchenko <annazubchenko282@gmail.com>
2024-06-07 19:07:38 +0300

    updated redux logic

--- src/redux/auth/authSlice.ts
+++ src/redux/auth/authSlice.ts
@@ -1,15 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { AuthState } from 'src/redux/types';
 
 const initialState: AuthState = {
   user: { name: '', email: '', role: '' },
-  token: '',
+  access_token: '',
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
+  reducers: {
+    setEmail: (state, action: PayloadAction<string>) => {
+      state.user.email = action.payload;
+    },
+    setToken: (state, action: PayloadAction<string>) => {
+      state.access_token = action.payload;
+    },
+    clearToken: state => {
+      state.access_token = '';
+    },
+  },
 });
 
+export const { setToken, clearToken, setEmail } = authSlice.actions;
 export const authReducer = authSlice.reducer;
