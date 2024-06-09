6958bb50067bc41e68c836a9204ef8ee7cc1c0bc
Mykhailo Fomenko <85066667+MishaFomenko@users.noreply.github.com>
2024-06-09 15:44:13 -0400

    fix: renamed input field folder

---
+++ src/components/inputField/styles.ts
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import { InputProps } from 'src/components/inputField/types';
+import { Theme } from 'src/styles/theme';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+  width: 335px;
+  height: 72px;
+`;
+
+export const Label = styled.label<{ theme?: Theme }>`
+  font-size: ${({ theme }) => theme.fontSize.md};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+export const Input = styled.input<
+  { hasError: boolean; theme?: Theme } & InputProps
+>`
+  width: 100%;
+  padding: 8px;
+  border-radius: 8px;
+  border-width: 1px;
+  border: solid;
+  border-color: ${({ hasError, theme }) =>
+    hasError ? theme?.colors.error_red : theme?.colors.border};
+  outline: none;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.black};
+  }
+`;
+
+export const Error = styled.span<{ theme?: Theme }>`
+  color: ${({ theme }) => theme.colors.error_red};
+  font-size: ${({ theme }) => theme.fontSize.sm};
+`;
