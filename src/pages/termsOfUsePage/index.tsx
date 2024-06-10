d19a880fe92abb5c2ffff5f8c3e17c02119251d5
AlexandraVoloshyna <oleksandravolosyna@gmail.com>
2024-06-10 10:16:57 +0300

    feat: add privacy policy and terms of use pages

--- src/pages/termsOfUsePage/index.tsx
+++ src/pages/termsOfUsePage/index.tsx
@@ -1,5 +1,48 @@
+import { MainHeading } from 'components/mainHeading';
+import {
+  MainTitle,
+  StyledSection,
+  Text,
+  Title,
+  Wrapper,
+} from 'components/sharedUI/text/styles';
+import { useTranslation } from 'react-i18next';
+
 export const TermsOfUsePage = () => {
-  return <div></div>;
+  const { t } = useTranslation();
+  return (
+    <>
+      <MainHeading>Terms & Conditions</MainHeading>
+      <StyledSection>
+        <Wrapper>
+          <div>
+            <MainTitle>{t('termsConditions.title')}</MainTitle>
+            <Text>{t('termsConditions.text')}</Text>
+          </div>
+          <div>
+            <Title>{t('termsConditions.title')}</Title>
+            <Text>{t('termsConditions.text')}</Text>
+          </div>
+          <div>
+            <Title>{t('termsConditions.title')}</Title>
+            <Text>{t('termsConditions.text')}</Text>
+          </div>
+          <div>
+            <Title>{t('termsConditions.title')}</Title>
+            <Text>{t('termsConditions.text')}</Text>
+          </div>
+          <div>
+            <Title>{t('termsConditions.title')}</Title>
+            <Text>{t('termsConditions.text')}</Text>
+          </div>
+          <div>
+            <Title>{t('termsConditions.title')}</Title>
+            <Text>{t('termsConditions.text')}</Text>
+          </div>
+        </Wrapper>
+      </StyledSection>
+    </>
+  );
 };
 
 export default TermsOfUsePage;
