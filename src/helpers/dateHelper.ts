0a533e4adf2e67c893dc36a3fd94385e7058e3b5
AlexandraVoloshyna <oleksandravolosyna@gmail.com>
2024-06-16 17:06:50 +0300

    refactor: modify date helper function to display correct date

--- src/helpers/dateHelper.ts
+++ src/helpers/dateHelper.ts
@@ -1,8 +1,8 @@
 export const formatDate = (date: Date): string => {
   const formattedDate = new Intl.DateTimeFormat('en-US', {
-    weekday: 'long',
     day: '2-digit',
     month: 'long',
+    year: 'numeric',
   }).format(new Date(date));
   return formattedDate;
 };
